1e4a5422844a468f0f16bd85accdc26c0db97888
Nelson Roberts <nroberts@sbmcorp.com>
2021-10-27 13:39:16 -0700

    added a missing alert on task type

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,35 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var taskFormHandler = function (event) {
+var taskFormHandler = function(event) {
   event.preventDefault();
-  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskNameInput = document.querySelector("input[name='task-name'").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-  // check if input values are empty strings
-  if (!taskNameInput || !taskTypeInput) {
+  // check if inputs are empty (validate)
+  if (taskNameInput === "" ) {
     alert("You need to fill out the task form!");
+   } else if (taskTypeInput === "")
+     alert("You need to fill out the task type!");
+   else{
     return false;
   }
+  
+  formEl.reset();
 
-  // package up data as an object
+  // reset form fields for next task to be entered
+  document.querySelector("input[name='task-name']").value = "";
+  document.querySelector("select[name='task-type']").selectedIndex = 0;
+
   var taskDataObj = {
     name: taskNameInput,
-    type: taskTypeInput,
+    type: taskTypeInput
   };
 
-  // send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
 };
 
-var createTaskEl = function (taskDataObj) {
+var createTaskEl = function(taskDataObj) {
   // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
@@ -30,15 +37,12 @@ var createTaskEl = function (taskDataObj) {
   // create div to hold task info and add to list item
   var taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
-  taskInfoEl.innerHTML =
-    "<h3 class='task-name'>" +
-    taskDataObj.name +
-    "</h3><span class='task-type'>" +
-    taskDataObj.type +
-    "</span>";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
   listItemEl.appendChild(taskInfoEl);
 
-  // add entire list item to list
+  console.dir(listItemEl);
+
+  // add list item to list
   tasksToDoEl.appendChild(listItemEl);
 };
 
